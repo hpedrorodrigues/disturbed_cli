a5f99e6b6e4824f92ef200f5cc1e9f8299b6fc18
Pedro Rodrigues <hpedrorodrigues@users.noreply.github.com>
2024-10-22 00:55:18 -0300

    Remove docstring

--- disturbed/configuration/types.py
+++ disturbed/configuration/types.py
@@ -18,17 +18,6 @@ class ScheduleOverride:
 
 @dataclass
 class ScheduleMapping:
-    """
-    Represents a schedule configuration linking an OpsGenie schedule to a Slack user group.
-
-    Attributes:
-        schedule_name (str): The name of the schedule in OpsGenie. This is used to query
-                             OpsGenie for the current on-call user.
-
-        user_group_name (str): The name of the Slack user group to be updated. This group will be updated
-                               with the current on-call user.
-    """
-
     schedule_name: str
     user_group_name: str
     overrides: Optional[list[ScheduleOverride]]
